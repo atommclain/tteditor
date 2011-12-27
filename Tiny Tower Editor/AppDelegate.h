9be4e22a057a9bc2d5846712540ac11d9d78ac43
Adam McLain <amclain@ubermind.com>
2011-12-26 19:40:07 -0800

    Initial Commit

---
+++ Tiny Tower Editor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tiny Tower Editor
+//
+//  Created by Adam Mclain on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tiny Tower Editor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tiny Tower Editor
+//
+//  Created by Adam Mclain on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
