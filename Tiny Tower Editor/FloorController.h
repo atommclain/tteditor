6f9f50136e6d718932ec0ab0fd0a414cd79456f9
Adam McLain <amclain@ubermind.com>
2012-01-16 17:21:13 -0800

    Removing dead code from FloorController.

--- Tiny Tower Editor/FloorController.h
+++ Tiny Tower Editor/FloorController.h
@@ -14,18 +14,8 @@
 @interface FloorController : NSObject
 
 @property (nonatomic, retain, readonly) Floor *floor;
-//- (id)initWithStrings:(NSArray *)floorStrings;
 - (id)initWithString:(NSString *)floorString;
-//- (id)initWithFloors:(NSArray *)floors;
 - (id)initWithFloor:(Floor *)floor;
-//- (NSString *)floorsAsString;
-
-
-//- (Floor *)floorAtStory:(NSUInteger)story;
-//- (BOOL)buildNewFloor;
-//- (BOOL)clearFloorAtStory:(NSUInteger)story;
-- (BOOL)clearFloor;
-//- (BOOL)moveFloor:(Floor *)floor toStory:(NSUInteger)story;
 - (BOOL)performTransformOnFloor:(FloorTransform *)transform;
 
 @end
